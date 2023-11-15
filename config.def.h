84c1e01d6c920b78e5fad56a7b9035ab9b00c2c7
Emma Casey <113342205+emmacasey326@users.noreply.github.com>
2023-11-15 17:19:52 +1000

    Patch applied: dwm-colorbar-6.3.diff

--- config.def.h
+++ config.def.h
@@ -22,10 +22,15 @@ static const char col_urgborder[]   = "#ff0000";
 static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
-	/*               fg         bg         border        */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2     },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan      },
-	[SchemeUrg]  = { col_gray4, col_cyan,  col_urgborder },
+	/*                   fg         bg         border        */
+	[SchemeNorm]     = { col_gray3, col_gray1, col_gray2     },
+	[SchemeSel]      = { col_gray4, col_cyan,  col_cyan      },
+	[SchemeUrg]      = { col_gray4, col_cyan,  col_urgborder },
+	[SchemeStatus]   = { col_gray3, col_gray1, "#000000"     }, // Statusbar right {text,background,not used but cannot be empty}
+	[SchemeTagsSel]  = { col_gray4, col_cyan,  "#000000"     }, // Tagbar left selected {text,background,not used but cannot be empty}
+	[SchemeTagsNorm] = { col_gray3, col_gray1, "#000000"     }, // Tagbar left unselected {text,background,not used but cannot be empty}
+	[SchemeInfoSel]  = { col_gray4, col_cyan,  "#000000"     }, // infobar middle  selected {text,background,not used but cannot be empty}
+	[SchemeInfoNorm] = { col_gray3, col_gray1, "#000000"     }, // infobar middle  unselected {text,background,not used but cannot be empty}
 };
 static const unsigned int alphas[][3]      = {
     /*               fg      bg        border      */
@@ -171,4 +176,3 @@ static const Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
