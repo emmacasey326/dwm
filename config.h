48ce2e77315bd04e32cf164b803b5e6a2a8ce0e3
Emma Casey <113342205+emmacasey326@users.noreply.github.com>
2023-11-15 18:28:54 +1000

    Made the default tags the custom icons instead of numbers.

--- config.h
+++ config.h
@@ -56,8 +56,8 @@ static const unsigned int alphas[][3]      = {
 };
 
 /* tagging */
-static const char *tags[]    = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-static const char *tagsalt[] = { "", "", "", "󰇮", "󰨇", "", "", "󰙯", "" };
+static const char *tags[]    = { "", "", "", "󰇮", "󰨇", "", "", "󰙯", "" };
+static const char *tagsalt[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 static const int momentaryalttags = 0; /* 1 means alttags will show only when key is held down */
 
 static const Rule rules[] = {
