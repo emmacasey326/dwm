320628029ca48cf5648946cf980902107ce55809
Emma Casey <113342205+emmacasey326@users.noreply.github.com>
2023-11-16 00:56:58 +1000

    Decreased horizontal tag underline padding

--- config.h
+++ config.h
@@ -66,7 +66,7 @@ static const char *tags[]    = { "", "", "", "󰇮", "", "󰑋", "�
 static const char *tagsalt[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 static const int momentaryalttags = 0; /* 1 means alttags will show only when key is held down */
 
-static const unsigned int ulinepad	= 4;	/* horizontal padding between the underline and tag */
+static const unsigned int ulinepad	= 2;	/* horizontal padding between the underline and tag */
 static const unsigned int ulinestroke	= 2;	/* thickness / height of the underline */
 static const unsigned int ulinevoffset	= 0;	/* how far above the bottom of the bar the line should appear */
 static const int ulineall 		= 0;	/* 1 to show underline on all tags, 0 for just the active ones */
